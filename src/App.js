01bdd75f1677377912ad2defb2ee3512fce16700
judabne <76679098+judabne@users.noreply.github.com>
2021-04-05 11:12:25 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ class App extends Component {
             <div>
                 <h5 style={{textAlign: "center"}}>Carousel</h5>
                 <Carousel infinite>
-                    <img src="https://images.all-free-download.com/images/graphicthumb/beautiful_scenery_02_hd_picture_166319.jpg" alt="sample image" />
                     <img src="https://images.unsplash.com/photo-1595835018349-198460e1d309?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8MTYlM0E5fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="sample image"/>
+                    <img src="https://images.all-free-download.com/images/graphicthumb/beautiful_scenery_02_hd_picture_166319.jpg" alt="sample image" />
+                    <img src="https://i.pinimg.com/originals/7b/c1/24/7bc12410da0124244008b81c5f9e37ec.jpg" alt="sample image"/>
                     <img src="https://www.online-tech-tips.com/wp-content/uploads/2016/03/widescreen-iphone-photo.jpg.optimal.jpg" alt="sample image"/>
                     <img src="https://wallpaperaccess.com/full/25637.jpg" alt="sample image"/>
                     <h1 style={{background: '#206ebd'}}>Sample h1</h1>
