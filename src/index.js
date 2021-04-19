4c15c17be430d70f503bb575fa0efecdfed5a591
judabne <76679098+judabne@users.noreply.github.com>
2021-04-19 16:29:40 +0300

    First Review Modifications

--- src/index.js
+++ src/index.js
@@ -4,9 +4,4 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-const app = (
-        <App />
-);
-
-
-ReactDOM.render(app, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
